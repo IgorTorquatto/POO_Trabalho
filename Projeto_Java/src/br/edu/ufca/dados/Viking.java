bb5a9533a105055a9df1ef7227024c97dcffca83
DESKTOP-G3FO92L\UFCA <A@A.COM>
2023-04-19 11:14:18 -0300

    Att main

--- Projeto_Java/src/br/edu/ufca/dados/Viking.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ufca.dados;
-
-public class Viking extends PersonagemAbstrato {
-	//Só ataca corpo a corpo, ++dano +vida -sofre mais dano
-	public Viking(String nome,int vida,int nivel,int ataque,double gold) {
-		super(nome,vida,nivel,ataque,gold);
-	}
-}
--- Projeto_Java/src/br/edu/ufca/teste/Teste.java
+++ Projeto_Java/src/br/edu/ufca/teste/Teste.java
@@ -3,10 +3,7 @@ package br.edu.ufca.teste;
 public class Teste {
 	//Esse package é onde fica a main
   public static void main(String[] args) {
-    String x = "teste";
-    System.out.println(x);
     
-    int h = 3;
-    System.out.println(h);
+	  
   }
 }
